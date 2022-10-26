dd8b93ce5e6ef5b89d0b8958ecaa949f897f680c
verteramofederico <verteramo.federico@gmail.com>
2022-10-26 13:46:12 +0200

    api modify news added

--- src/components/api/controller.js
+++ src/components/api/controller.js
@@ -1,9 +1,12 @@
 let newsModels = require('../../models/newsModels')
 let usersModels = require('../../models/usersModels')
 const jwt = require('jsonwebtoken')
+const nodemailer = require("nodemailer");
 
-
+let util = require('util')
 let cloudinary = require('cloudinary').v2
+
+const destroy = util.promisify(cloudinary.uploader.destroy)
 
 module.exports = { 
     async getNews (req, res) {
@@ -58,6 +61,56 @@ module.exports = {
         } catch (e) {
             console.log(e)
             res.status(500).send({ Error: e })
+        }
+    }, async Contact (req, res) {
+        const mail = {
+            to: 'verteramo.federico@gmail.com',
+            subject: "Contacto web restaurant UTN",
+            html: `${req.body.nombre} quiere mas informacion al correo:
+            ${req.body.email} <br> 
+            ${req.body.mensaje}`
+        }  
+        const transport = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS
+            }
+        })
+        await transport.sendMail(mail)
+        res.status(201).json({
+            error: false,
+            message: "mensaje enviado"
+        })
+    }, async modifyNewById (req, res) {
+        console.log(req.body)
+        try {    
+            let img_id = req.body.img_original
+            let delete_img_old = false
+            if (req.body.img_delete === '1') {
+                img_id = null
+                delete_img_old = true
+            } 
+            if (delete_img_old && req.body.img_original) {
+                await (destroy(req.body.img_original))
+            }
+    
+            let obj = {
+                title: req.body.title,
+                subtitle: req.body.subtitle,
+                body: req.body.body,
+                img_id
+            }
+            const data = await newsModels.modifyNewById(obj, req.body.id);
+            res.status(201).json(data)
+        }
+        catch (Error) {
+            console.log(Error, "error catch")
+            res.render('modify', { 
+                error: true, 
+                message: 'No se modifico la novedad' 
+            })
         }
     },  async Test (req, res) {
         res.status(200).json("ESTA INFORMACION ES ENVIADA DESDE EL BACK LUEGO DE CHEQUEAR EL TOKEN CON JWTTOKEN.")
--- src/components/api/network.js
+++ src/components/api/network.js
@@ -6,6 +6,8 @@ const auth = require('../../middlewares/auth')
 
 router.get('/novedades',controller.getNews)
 router.post('/login',controller.Login)
+router.post('/modificar', auth, controller.modifyNewById)
+router.post('/contacto',controller.Contact)
 router.get('/test', auth,controller.Test)
 
 module.exports = router
